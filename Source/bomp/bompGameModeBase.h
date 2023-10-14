98e2f1658903400e5c8f9ecc7cc21be826d1f2f6
TomoshigeKosei <22cu0224@jec.ac.jp>
2023-10-14 14:42:35 +0900

    最初のコミット
    UE4の初期プロジェクトをコミット

---
+++ Source/bomp/bompGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "bompGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BOMP_API AbompGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
