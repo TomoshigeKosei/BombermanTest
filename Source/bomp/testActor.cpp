9a3084cbf09a4f9d8a5ca337b639fde94f7a0936
TomoshigeKosei <22cu0224@jec.ac.jp>
2023-10-14 15:04:02 +0900

    [add]新しいレベルの作成、まっすぐ進み続けるc++クラスの作成

---
+++ Source/bomp/testActor.cpp
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "testActor.h"
+
+// Sets default values
+AtestActor::AtestActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AtestActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AtestActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	//アクタの座標を取得
+	FVector playerPos = GetActorLocation();
+	//アクタのx座標を増やす
+	playerPos.X += 5.f * DeltaTime;
+	//アクタの座標を更新
+	SetActorLocation(playerPos);
+}
+
